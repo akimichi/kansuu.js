924a3e9e7471d21b3fd288c5e649fdbf572f1878
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2015-03-27 19:24:53 +0900

    kensho.spec successful

--- test/kensho.spec.js
+++ test/kensho.spec.js
@@ -6,9 +6,13 @@ var qc = require('../lib/kensho.js');
 
 describe("Kensho module", function() {
   it("ints", function(next) {
-	expect(function(){
-	  var ints = qc.
-	})
+	var ints = qc.ints(0);
+	expect(
+	  ints.value
+	).to.eql(0);
+	expect(
+	  ints.next().value
+	).to.eql(1);
 	next();
   });
 });
