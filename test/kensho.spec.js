1bdcf4330cc5adfb40363eda99c83f5b4de8e39d
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2015-03-27 17:00:39 +0900

    create kensho.spec.js

---
+++ test/kensho.spec.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var expect = require('expect.js');
+var __ = require('../lib/kansuu.js');
+var qc = require('../lib/kensho.js');
+
+describe("Kensho module", function() {
+  it("ints", function(next) {
+	expect(function(){
+	  var ints = qc.
+	})
+	next();
+  });
+});
+
