fbdc8accb1ca4a7beccc99cecc62a0e351584cdb
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2015-07-22 23:56:17 +0900

    defined add in interpreter example

--- examples/interpreter.js
+++ examples/interpreter.js
@@ -14,6 +14,39 @@ var hasProp = {}.hasOwnProperty;
 
 module.exports = {
   ordinary: {
+    unit: (value) => {
+      return __.monad.identity.unit.call(__,value);
+    },
+    flatMap: (instance) => {
+      var self = this;
+      return (transform) => {
+        expect(transform).to.a('function');
+        return transform.call(self, instance);
+      };
+    },
+    apply: (rator) => {
+      var self = this;
+      return (rand) => {
+        if(__.typeOf(rator) === 'function'){
+          return self.ordinary.unit.call(self,rator.call(self,rand));
+        } else {
+          return self.ordinary.unit.call(self,undefined);
+        }
+      };
+    },
+    add: (n) => {
+      var self = this;
+      expect(n).to.be.a('number');
+      return (m) => {
+        expect(m).to.be.a('number');
+        //if(m.subtype === 'number' && n.subtype === 'number' ) {
+        if(__.isNumber(m) && __.isNumber(n)) {
+          return self.ordinary.unit.call(self,m + n);
+        } else {
+          return self.ordinary.unit.call(self,undefined);
+        }
+      };
+    },
     evaluate: (exp) => {
       var self = this;
       expect(exp.type).to.eql('exp');
@@ -21,7 +54,35 @@ module.exports = {
         __.list.censor.call(__,env);
         switch (exp.subtype) {
         case 'variable':
-          return self.ordinary.env.lookup(exp.content, env);
+          return self.ordinary.env.lookup.call(self,exp.content, env);
+        case 'number':
+          return self.ordinary.unit.call(self,exp.content);
+        case 'add':
+          var rator = exp.content.rator;
+          var rand =  exp.content.rand;
+          expect(rator.type).to.eql('exp');
+          expect(rand.type).to.eql('exp');
+          return self.ordinary.flatMap.call(self,self.ordinary.evaluate.call(self,rator)(env))(function (m) {
+            return self.ordinary.flatMap.call(self,self.ordinary.evaluate.call(self,rand)(env))(function (n) {
+              return self.ordinary.add.call(self, m)(n);
+            });
+          });
+        case 'lambda':
+          var closure = (arg) => {
+            var newEnv = self.ordinary.env.extend.call(self, __.pair.mkPair.call(__,exp.content.arg)(arg),env);
+            return self.ordinary.evaluate.call(self,exp.content.body)(newEnv);
+          };
+          return self.ordinary.unit.call(self,closure);
+        case 'application':
+          var rator = exp.content.rator;
+          var rand =  exp.content.rand;
+          self.ordinary.flatMap.call(self,self.ordinary.evaluate.call(self,rator)(env))(function (f) {
+            return self.ordinary.flatMap.call(self,self.ordinary.evaluate.call(self,rand)(env))(function (a) {
+              return self.ordinary.apply.call(self, f)(a);
+            });
+          });
+        default:
+          throw new Error("evaluate should accept expressions");
         }
       };
     },
@@ -32,33 +93,76 @@ module.exports = {
           subtype: 'variable',
           content: name
         };
+      },
+      number: (n) => {
+        return {
+          type: 'exp',
+          subtype: 'number',
+          content: n
+        };
+      },
+      add: (n) => {
+        return (m) => {
+          return {
+            type: 'exp',
+            subtype: 'add',
+            content: {
+              rator: n,
+              rand: m
+            }
+          };
+        };
+      },
+      lambda: (name) => {
+        return (exp) => {
+          return {
+            type: 'exp',
+            subtype: 'lambda',
+            content: {
+              name: name,
+              body: exp
+            }
+          };
+        };
+      },
+      app: (rator) => {
+        return (rand) => {
+          return {
+            type: 'exp',
+            subtype: 'application',
+            content: {
+              rator: rator,
+              rand: rand
+            }
+          };
+        };
       }
     },
     data: {
-      unit : (value) => {
-        switch (__.typeOf(value)) {
-        case 'number':
-          return {
-            type: 'data',
-            subtype: 'number',
-            content: value
-          };
-        case 'function':
+      number: (value) => {
+        return {
+          type: 'data',
+          subtype: 'number',
+          content: value
+        };
+      },
+      error: {
+        type: 'data',
+        subtype: 'error',
+        content: void(0)
+      },
+      fun: (arg) => {
+        return (body) => {
           return {
             type: 'data',
             subtype: 'function',
-            content: value
+            content: {
+              arg: arg,
+              body: body
+            }
           };
-        case 'undefined':
-          return {
-            type: 'data',
-            subtype: 'error',
-            content: value
-          };
-        default:
-          throw new Error();
-        }
-      },
+        };
+      }
     },
     // environment
     // ~~~haskell
@@ -66,9 +170,6 @@ module.exports = {
     // ~~~
     env: {
       empty: __.list.empty,
-      // empty: (() => {
-      //   return __.monad.list.empty;
-      // })(),
       extend: (pair, oldenv) => {
         __.pair.censor.call(__,pair);
         __.list.censor.call(__,oldenv);
@@ -77,12 +178,12 @@ module.exports = {
       lookup: (name, env) => {
         var self = this;
         if(__.list.isEmpty.call(__,env)) {
-          return __.monad.maybe.nothing;
+          return self.ordinary.unit.call(self,undefined);
         } else {
-          return __.list.foldr.call(__,env)(__.monad.maybe.nothing)((item) => {
+          return __.list.foldr.call(__,env)(self.ordinary.unit.call(self,undefined))((item) => {
             return (accumulator) => {
               if(item.left === name) {
-                return __.monad.maybe.unit.call(__,item.right);
+                return self.ordinary.unit.call(self,item.right);
               } else {
                 return accumulator;
               }
--- test/examples-interpreter.spec.js
+++ test/examples-interpreter.spec.js
@@ -7,24 +7,48 @@ var intp = require('../examples/interpreter.js');
 
 describe("'interpreter' example", () => {
   describe("ordinary", () => {
-    it('can lookup env', (next) => {
-      var env = intp.ordinary.env.extend.call(intp, __.pair.mkPair.call(__,"a")(1),intp.ordinary.env.empty);
-      expect(
-        intp.ordinary.env.lookup.call(intp,"a", env)
-      ).to.eql(
-        __.monad.maybe.unit.call(__,1)
-      );
-      next();
+    describe("environment", () => {
+      it('can lookup env', (next) => {
+        var env = intp.ordinary.env.extend.call(intp, __.pair.mkPair.call(__,"a")(1),intp.ordinary.env.empty);
+        expect(
+          intp.ordinary.env.lookup.call(intp,"a", env)
+        ).to.eql(
+          intp.ordinary.unit.call(intp,1)
+        );
+        next();
+      });
     });
-    it('can evaluate variable', (next) => {
-      var exp = intp.ordinary.exp.variable("a");
-      var env = intp.ordinary.env.extend.call(intp, __.pair.mkPair.call(__,"a")(1),intp.ordinary.env.empty);
-      expect(
-        intp.ordinary.evaluate.call(intp,exp)(env)
-      ).to.eql(
-        __.monad.maybe.unit.call(__,1)
-      );
-      next();
+    describe("evaluate", () => {
+      it('can evaluate variable', (next) => {
+        var exp = intp.ordinary.exp.variable("a");
+        var env = intp.ordinary.env.extend.call(intp, __.pair.mkPair.call(__,"a")(1),intp.ordinary.env.empty);
+        expect(
+          intp.ordinary.evaluate.call(intp,exp)(env)
+        ).to.eql(
+          intp.ordinary.unit.call(intp,1)
+        );
+        next();
+      });
+      it('can evaluate number', (next) => {
+        var exp = intp.ordinary.exp.number(2);
+        expect(
+          intp.ordinary.evaluate.call(intp,exp)(intp.ordinary.env.empty)
+        ).to.eql(
+          intp.ordinary.unit.call(intp,2)
+        );
+        next();
+      });
+      it('can evaluate add', (next) => {
+        var n = intp.ordinary.exp.number(2);
+        var m = intp.ordinary.exp.number(3);
+        var exp = intp.ordinary.exp.add(n)(m);
+        expect(
+          intp.ordinary.evaluate.call(intp,exp)(intp.ordinary.env.empty)
+        ).to.eql(
+          intp.ordinary.unit.call(intp,5)
+        );
+        next();
+      });
     });
   });
 });
