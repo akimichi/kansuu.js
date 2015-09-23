a07dbca095bfd097c6c99cda42d66cd58b3a0391
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2015-09-24 00:37:06 +0900

    fix combinator example test

--- test/examples-combinator.spec.js
+++ test/examples-combinator.spec.js
@@ -6,20 +6,103 @@ var base = require('../lib/kansuu-base.js');
 var math = require('../lib/kansuu-math.js');
 
 describe("'combinator' example", () => {
-  // it('temp', (next) => {
-  // 	var data = {
-  // 	  temp: 24,
-  // 	  time: new Date("2013/2/15")
-  // 	};
-  // 	var judge = (data) => {
-  // 	  var temp = __.objects.get('temp')(data);
-  // 	  return __.and(math.isMoreThan(20))(math.isLessThan(30));
-  // 	};
-  //   expect(
-  //     judge(data)
-  //   ).to.eql(
-  // 	  true
-  // 	)
-  //   next();
-  // });
+  it('temp', (next) => {
+	var not = (predicate) => {
+	  return (data) => {
+		return ! predicate(data);
+	  }
+	};
+	var and = (predicate1) => {
+      return (predicate2) => {
+		return (data) => {
+		  return predicate1(data) && predicate2(data);
+		};
+	  };
+	};
+	var or = (predicate1) => {
+      return (predicate2) => {
+		return (data) => {
+		  return predicate1(data) || predicate2(data);
+		};
+      };
+	};
+	var within = (lower) => {
+	  return (upper) => {
+		return (data) => {
+		  return (extractor) => {
+			// return __.and(math.isMoreThan(lower))(math.isLessThan(upper))(extractor(data));
+			return seq(extractor, math.isMoreThan(lower))(extractor, math.isLessThan(upper))(data);
+		  };
+		};
+	  };
+	};
+	var seq = (firstExtractor, firstPredicate) => {
+		return (nextExtractor, nextPredicate) => {
+		  return (data) => {
+			var firstResult = firstPredicate(firstExtractor(data))
+			if(! firstResult) {
+			  return false;
+			} else {
+			  return nextPredicate(nextExtractor(data));
+			}
+		  }
+		};
+	};
+	var alt = (firstExtractor, firstPredicate) => {
+		return (nextExtractor, nextPredicate) => {
+		  return (data) => {
+			var firstResult = firstPredicate(firstExtractor(data))
+			if(firstResult) {
+			  return true;
+			} else {
+			  return nextPredicate(nextExtractor(data));
+			}
+		  }
+		};
+	};
+  	var data = {
+  	  temp: 24,
+  	  time: new Date("2013/2/15 17:57:27")
+  	};
+    expect(((_) => {
+	  var getTemp = (data) => {
+		return __.objects.get('temp')(data);
+	  };
+	  var getHour = (data) => {
+		return __.objects.get('time')(data).getHours();
+	  };
+	  return seq(getTemp, math.isMoreThan(20))(getHour, __.equal(17))(data)
+    })()).to.eql(
+  	  true
+  	)
+    expect(((_) => {
+	  var getTemp = (data) => {
+		return __.objects.get('temp')(data);
+	  };
+	  var getHour = (data) => {
+		return __.objects.get('time')(data).getHours();
+	  };
+	  return alt(getTemp, math.isMoreThan(30))(getHour, __.equal(17))(data)
+    })()).to.eql(
+  	  true
+  	)
+    expect(
+	  within(20)(30)(__.objects.get('temp')(data))(__.id)
+    ).to.eql(
+  	  true
+  	)
+    expect(
+	  within(20)(30)(data)(__.objects.get('temp'))
+    ).to.eql(
+  	  true
+  	)
+    expect(
+	  within(20)(30)(data)((data) => {
+		return __.objects.get('temp')(data)
+	  })
+    ).to.eql(
+  	  true
+  	)
+    next();
+  });
 });
