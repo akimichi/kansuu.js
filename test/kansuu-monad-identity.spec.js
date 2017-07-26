3b88b43d814eed73030ead4fdca42d45d01be5ec
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2017-07-26 17:40:49 +0900

    ID monad test succesful

--- test/kansuu-monad-identity.spec.js
+++ test/kansuu-monad-identity.spec.js
@@ -1,45 +1,41 @@
 "use strict";
 
-// var util = require('util');
-// var expect = require('expect.js');
-// var __ = require('../lib/kansuu.js');
-// var math = require('../lib/kansuu-math.js');
+var util = require('util');
+var expect = require('expect.js');
+var __ = require('../lib/kansuu.js');
+const ID = require('../lib/kansuu-monad.js').identity;
 // var seedrandom = require('seedrandom');
 // var Random = require("random-js");
 // var rng = Random.engines.mt19937();
 
-// describe("'monad' module", function() {
-//   describe("'identity' monad", () => {
-//     var unit = __.monad.identity.unit.bind(__);
-//     var flatMap = __.monad.identity.flatMap.bind(__);
-//     it("identity#flatMap", (next) => {
-//       var instance = unit(1);
-//       expect(
-//         flatMap(instance)((n) => {
-//           return unit(n * 2);
-//         })
-//       ).to.eql(
-//         unit(2)
-//       );
-//       expect(
-//         flatMap(instance)((n) => {
-//           return flatMap(unit(n * 2))((m) => {
-//             return unit(m * 3);
-//           });
-//         })
-//       ).to.eql(
-//         unit(6)
-//       );
-//       expect(
-//         flatMap(instance)((n) => {
-//           return flatMap(unit(n))((m) => {
-//             return unit(m * n);
-//           });
-//         })
-//       ).to.eql(
-//         unit(1)
-//       );
-//       next();
-//     });
-//   });
-// });
+describe("ID monad", () => {
+  it("ID#flatMap", (next) => {
+    var instance = ID.unit(1);
+    expect(
+      ID.flatMap(instance)((n) => {
+        return ID.unit(n * 2);
+      })
+    ).to.eql(
+     ID.unit(2)
+    );
+    expect(
+      ID.flatMap(instance)((n) => {
+        return ID.flatMap(ID.unit(n * 2))((m) => {
+          return ID.unit(m * 3);
+        });
+      })
+    ).to.eql(
+      ID.unit(6)
+    );
+    expect(
+      ID.flatMap(instance)((n) => {
+        return ID.flatMap(ID.unit(n))((m) => {
+          return ID.unit(m * n);
+        });
+      })
+    ).to.eql(
+      ID.unit(1)
+    );
+    next();
+  });
+});
