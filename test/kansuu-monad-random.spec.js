e3e7383ea817be219814561e0e4dc3a9765052e4
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2017-07-29 01:37:08 +0900

    cleanup

--- test/kansuu-monad-random.spec.js
+++ test/kansuu-monad-random.spec.js
@@ -13,12 +13,6 @@ const RandomJs = require("random-js"),
 var seedrandom = require('seedrandom');
 
 describe("'random' monad", function() {
-  // var int = __.monad.random.unit.bind(__)(0);
-  // var ns = __.monad.random.flatMap.bind(__)(int)(x => {
-  //   return __.monad.random.flatMap.bind(__)(int)(y => {
-  //  return
-  //   }
-  // })
   it("random.int", (next) => {
     //var rng = seedrandom("seed");
     rng.seed("seed");
--- test/kansuu-monad.spec.js
+++ test/kansuu-monad.spec.js
@@ -9,58 +9,6 @@ var Random = require("random-js");
 var rng = Random.engines.mt19937();
 
 describe("'monad' module", function() {
-  // describe("'random' monad", function() {
-  //   // var int = __.monad.random.unit.bind(__)(0);
-  //   // var ns = __.monad.random.flatMap.bind(__)(int)(x => {
-  //   //   return __.monad.random.flatMap.bind(__)(int)(y => {
-  //   //  return
-  //   //   }
-  //   // })
-  //   it("random.int", function(next){
-  //     //var rng = seedrandom("seed");
-  //     rng.seed("seed");
-  //     var intRandom = __.monad.random.int.bind(__)(rng);
-  //     expect(
-  //       intRandom.left
-  //     ).to.eql(
-  //         -1937831252
-  //     );
-  //     //var intRandom2 = __.monad.random.int.bind(__)(intRandom.right);
-  //     expect(
-  //       __.monad.random.int.bind(__)(intRandom.right).left
-  //     ).to.eql(
-  //         -884076225
-  //     );
-  //     expect(
-  //       __.monad.random.int.bind(__)(intRandom.right).left
-  //     ).to.eql(
-  //         -505527131
-  //     );
-  //     // expect(
-  //     //     __.monad.random.int.bind(__)(intRandom.right()).left
-  //     // ).to.eql(
-  //     //    2
-  //     //    //4.612568818010603e+306
-  //     // );
-  //     next();
-  //   });
-  //   it("random.ints", function(next){
-  //     var rng = Random.engines.mt19937();
-  //     rng.seed("seed");
-  //     var ints = __.monad.random.ints.bind(__)(3)(rng);
-  //     expect(
-  //       __.list.length.bind(__)(ints.left)
-  //     ).to.eql(
-  //       3
-  //     );
-  //     expect(
-  //       __.list.toArray.bind(__)(ints.left)
-  //     ).to.eql(
-  //       [ -1937831252, -884076225, -725654408 ]
-  //     );
-  //     next();
-  //   });
-  // });
   // describe("Readerモナド",() => {
   //   it("add10", (next) => {
   //     // main = print $ runReader add10 1
