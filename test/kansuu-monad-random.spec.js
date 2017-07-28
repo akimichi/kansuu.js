f0b21e62361f384fdb0f9492926ce07fdca53687
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2017-07-29 01:35:23 +0900

    Random monad

--- test/kansuu-monad-random.spec.js
+++ test/kansuu-monad-random.spec.js
@@ -4,11 +4,13 @@ var util = require('util');
 var expect = require('expect.js');
 var __ = require('../lib/kansuu.js');
 var math = require('../lib/kansuu-math.js');
-const List = require('../lib/kansuu-list.js');
-const Maybe = require('../lib/kansuu-monad.js').maybe;
-const Random = require("random-js");
+const List = require('../lib/kansuu-list.js'),
+  Maybe = require('../lib/kansuu-monad.js').maybe,
+  Pair = require('../lib/kansuu-pair.js'),
+  Random = require('../lib/kansuu-monad.js').random;
+const RandomJs = require("random-js"),
+ rng = RandomJs.engines.mt19937();
 var seedrandom = require('seedrandom');
-var rng = Random.engines.mt19937();
 
 describe("'random' monad", function() {
   // var int = __.monad.random.unit.bind(__)(0);
@@ -17,26 +19,26 @@ describe("'random' monad", function() {
   //  return
   //   }
   // })
-  it("random.int", function(next){
+  it("random.int", (next) => {
     //var rng = seedrandom("seed");
     rng.seed("seed");
-    var intRandom = __.monad.random.int.bind(__)(rng);
+    var intRandom = Random.int(rng);
     expect(
-      intRandom.left
+      Pair.left(intRandom)
     ).to.eql(
       -1937831252
     );
-    //var intRandom2 = __.monad.random.int.bind(__)(intRandom.right);
-    expect(
-      __.monad.random.int.bind(__)(intRandom.right).left
-    ).to.eql(
-      -884076225
-    );
-    expect(
-      __.monad.random.int.bind(__)(intRandom.right).left
-    ).to.eql(
-      -505527131
-    );
+    // //var intRandom2 = __.monad.random.int.bind(__)(intRandom.right);
+    // expect(
+    //   __.monad.random.int.bind(__)(intRandom.right).left
+    // ).to.eql(
+    //   -884076225
+    // );
+    // expect(
+    //   __.monad.random.int.bind(__)(intRandom.right).left
+    // ).to.eql(
+    //   -505527131
+    // );
     // expect(
     //     __.monad.random.int.bind(__)(intRandom.right()).left
     // ).to.eql(
@@ -45,17 +47,17 @@ describe("'random' monad", function() {
     // );
     next();
   });
-  it("random.ints", function(next){
-    var rng = Random.engines.mt19937();
+  it("random.ints", (next) => {
+    const rng = RandomJs.engines.mt19937();
     rng.seed("seed");
-    var ints = __.monad.random.ints.bind(__)(3)(rng);
+    const ints = Random.ints(3)(rng);
     expect(
-      __.list.length.bind(__)(ints.left)
+      List.length(Pair.left(ints))
     ).to.eql(
       3
     );
     expect(
-      __.list.toArray.bind(__)(ints.left)
+      List.toArray(Pair.left(ints))
     ).to.eql(
       [ -1937831252, -884076225, -725654408 ]
     );
