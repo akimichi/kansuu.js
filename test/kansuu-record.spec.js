6e16ec237bc8957fe39ef2afdb1b2941ed090225
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2017-08-19 08:31:34 +0900

    update record spec

--- test/kansuu-record.spec.js
+++ test/kansuu-record.spec.js
@@ -2,41 +2,42 @@
 
 const expect = require('expect.js'),
  __ = require('../lib/kansuu.js'),
- Record = require('../lib/kansuu-record.js'),
+ List = require('../lib/kansuu.js').monad.list,
+ Maybe = require('../lib/kansuu.js').monad.maybe,
+ Record = require('../lib/kansuu-record.js'); 
 
 
-describe("'record' module", function() {
-  var toArray = __.list.toArray.bind(__);
-  var some = function(n){
-    return __.monad.maybe.unit.bind(__)(n);
+describe("'record' module", () => {
+  const toArray = List.toArray,
+    nothing = Maybe.nothing;
+  var some = (n) => {
+    return Maybe.unit(n);
   };
-  var nothing = __.monad.maybe.nothing;
-  it("'record'", function(next) {
-    /* #@range_begin(record_module_test) */
-    expect(function(){
-      return __.record.empty.bind(__)(1);
-    }()).to.eql(
-      nothing
-    );
-    expect(function(){
-      var array = __.record.extend.bind(__)(__.record.empty.bind(__))(0)('a');
-      return array(0);
-    }()).to.eql(
+
+  it("'record'", (next) => {
+    // expect(function(){
+    //   return Record.empty(1);
+    // }()).to.eql(
+    //   nothing
+    // );
+    expect(() => {
+      const record = Record.set('a',0);
+      return Record.get('a');
+    })()).to.eql(
       some('a')
     );
+    // expect(function(){
+    //   var array = __.record.extend.bind(__)(__.record.extend.bind(__)(__.record.empty.bind(__))(0)('a'))(1)('b');
+    //   return array(1);
+    // }()).to.eql(
+    //   some('b')
+    // );
     expect(function(){
-      var array = __.record.extend.bind(__)(__.record.extend.bind(__)(__.record.empty.bind(__))(0)('a'))(1)('b');
-      return array(1);
-    }()).to.eql(
-      some('b')
-    );
-    expect(function(){
-      var object = __.record.extend.bind(__)(__.record.empty.bind(__))('key')('value');
-      return object('key');
-    }()).to.eql(
-      some('value')
-    );
-    /* #@range_end(record_module_test) */
+      // var object = __.record.extend.bind(__)(__.record.empty.bind(__))('key')('value');
+      // return object('key');
+    // }()).to.eql(
+      // some('value')
+    // );
     next();
   });
 });
