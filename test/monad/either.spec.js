9582be03218d46d273afb7b30dcbc0873d009142
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2017-08-26 13:04:09 +0900

    version 0.2.9

--- test/monad/either.spec.js
+++ test/monad/either.spec.js
@@ -3,12 +3,10 @@
 const util = require('util'),
   expect = require('expect.js'),
   __ = require('../../lib/kansuu.js'),
-  Either = require('../../lib/kansuu-monad.js').either;
-
+  Either = require('../../lib/kansuu.js').monad.either;
 
 // ### Eitherモナドのテスト
 describe("Eitherモナドをテストする",() => {
-  // 数値のときだけ計算が成功するテスト
   it("数値のときだけ計算が成功するテスト", (next) => {
     Either.match(Either.flatMap(Either.left("wrong"))((n) => {
       return Either.unit(n + 1);
@@ -17,11 +15,7 @@ describe("Eitherモナドをテストする",() => {
         expect().fail();
       },
       left: (value) => {
-        expect(
-          value
-        ).to.eql(
-          "wrong"
-        );
+        expect(value).to.eql("wrong");
       }
     });
     Either.match(Either.flatMap(Either.unit(2))((n) => {
